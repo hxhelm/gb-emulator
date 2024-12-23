2eb33621fc56c9b21c83cd8b7255f72f9dbf671e
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2024-12-23 14:40:48 +0100

    fix timers

--- src/cpu/timers.rs
+++ src/cpu/timers.rs
@@ -9,7 +9,7 @@ pub struct Clock {
 impl Clock {
     pub(crate) fn increment(&mut self, t_cycles: u8) {
         assert_eq!(t_cycles % 4, 0, "t_cycles need to be a multiple of 4");
-        self.t = self.m.wrapping_add(t_cycles.into());
+        self.t = self.t.wrapping_add(t_cycles.into());
         self.m = self.t / 4;
     }
 }
@@ -23,7 +23,7 @@ impl CPU {
         (self.read_tac() & 0x04) != 0
     }
 
-    fn get_timer_t_frequency(&self) -> u64 {
+    fn get_timer_m_frequency(&self) -> u64 {
         const TIMER_FREQUENCIES: [u64; 4] = [256, 4, 16, 64];
         TIMER_FREQUENCIES[(self.read_tac() & 0x03) as usize]
     }
@@ -45,16 +45,16 @@ impl CPU {
             return;
         }
 
-        let timer_frequency = self.get_timer_t_frequency();
+        let timer_frequency = self.get_timer_m_frequency();
+        let timer_diff = self.clock.m.saturating_sub(self.last_timer_update);
 
-        let timer_diff = self.clock.t.saturating_sub(self.last_timer_update);
         if timer_diff >= timer_frequency {
             let increments = timer_diff / timer_frequency;
             let (tima, did_overflow) = self.read_tima().overflowing_add(increments as u8);
 
             if did_overflow {
                 self.write_tima(self.read_tma());
-                // self.bus.request_timer_interrupt();
+                self.bus.request_timer_interrupt();
             } else {
                 self.write_tima(tima);
             }
