b48005973ee667e4bda28bb8c55c793d308c7444
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2025-07-27 23:32:47 +0200

    sort roms in metadata script

--- roms/print-metadata.sh
+++ roms/print-metadata.sh
@@ -1,5 +1,5 @@
 printf "%-60s %-10s %-10s %-10s\n" "ROM Path" "MBC" "ROM Size" "RAM Size"
-find . -type f -name "*.gb" | sed 's|^\./||' | while read -r f; do
+find . -type f -name "*.gb" | sed 's|^\./||' | sort | while read -r f; do
   read -r mbc rom ram <<< $(xxd -ps -s 0x0147 -l 3 "$f" | sed 's/\(..\)/0x\1 /g')
   printf "%-60s %-10s %-10s %-10s\n" "$f" "$mbc" "$rom" "$ram"
 done
